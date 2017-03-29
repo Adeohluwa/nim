c62b559ace80c2957271d5734d555b2dc828510f
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-03-29 18:18:15 +0100

    function definitions with a little dabble into generics

---
+++ procedures.nim
@@ -0,0 +1,18 @@
+#what the rest of the world calls functions or methods
+#you cant have typeless parameters
+
+proc add(x,y: int): int =
+  #this mystical result var is an implicit return
+  result = x+y
+
+
+#and oh you have generics but maybe those in full later
+
+
+proc add(x,y: int | float): int =
+  #means it can accept either an int or float
+  #not sure tnis will compile ;)
+  result = x+y
+
+
+echo add(2,3)
