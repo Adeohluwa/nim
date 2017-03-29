ccef45b1453e98b3074bb7d3e49e1c9c66dcc5a8
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-03-29 18:07:15 +0100

    homogenous sequences in nim

---
+++ lists.nim
@@ -0,0 +1,17 @@
+#type is inferred if you omit it
+#@[] is sequence constructor similar to lists in python
+#nim seqs are homogenous
+
+var
+  cities = @["New York", "Lagos", "Berlin"]
+  
+#iterating through a seq 
+
+for eachCity in cities:
+  echo eachCity
+
+#and oh listcomps like in haskell & py
+#
+import future
+
+lc[each, (each <- cities, len(cities) <=6), string]
