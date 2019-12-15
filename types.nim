c0ba845cafb8d01e203fbe0b1aafd6bd33269eec
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-15 17:06:52 +0100

    Create custom data types

---
+++ types.nim
@@ -0,0 +1,18 @@
+# object type
+
+type 
+  Human = object
+    name: string
+    age: int
+
+# procedure that accepts the data type
+proc shout(person: Human): string =
+  echo "!!!"
+
+let ade = Human(
+  name: "adeoluwa adejumo",
+  age: 99
+)
+
+# uniform call syntax
+echo ade.shout()
