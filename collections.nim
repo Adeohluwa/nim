b8abe4a73a0599d729f6a2f64e1efe9f510d9a72
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-15 17:51:21 +0100

    Collections

---
+++ collections.nim
@@ -0,0 +1,14 @@
+# arrays
+# homogenous, length known @ compile type
+# performant
+[1,2,3,4]
+
+
+
+
+# sequences
+# flexible, most versaitle container i will be using 
+@[1,2,3,4,5]
+
+# &      concat lists
+# [^1]   last item
